0e5f487607f39e94dcb224b386436a240e7bff6b
KovalchukDanil0 <105009706+KovalchukDanil0@users.noreply.github.com>
2024-08-28 08:42:53 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { Plugin, ResolvedConfig } from "vite";
+import { mkdir, readFile, writeFile } from "fs/promises";
+import { join, parse } from "path";
+
+export type CopyAndTransformPluginType = {
+  srcPath: string;
+  transformedProps: () => Promise<{
+    [key: string]: string | object | undefined;
+  }>;
+};
+
+export default function viteCopyTransformJson({
+  srcPath,
+  transformedProps,
+}: CopyAndTransformPluginType): Plugin {
+  let config: ResolvedConfig;
+
+  return {
+    name: "vite-plugin-transform-json",
+    apply: "build", // Apply only during build
+
+    configResolved(tempResolve) {
+      config = tempResolve;
+    },
+
+    async generateBundle() {
+      const destDir = config.build.outDir;
+      const fileName = parse(srcPath).base;
+      const destPath = join(destDir, fileName);
+
+      try {
+        // Read the original file.json
+        const content = await readFile(srcPath, "utf-8");
+
+        const originalJson = JSON.parse(content);
+        const resolvedProps = await transformedProps();
+
+        // Merge with additional data
+        const transformedJson = {
+          ...resolvedProps,
+          ...originalJson,
+        };
+
+        // Ensure the destination directory exists
+        await mkdir(destDir, { recursive: true });
+
+        // Write the transformed file.json
+        await writeFile(destPath, JSON.stringify(transformedJson), "utf-8");
+
+        console.log(`${fileName} has been copied and transformed.`);
+      } catch (error) {
+        console.error(
+          `Failed to copy and transform ${fileName}: ${
+            (error as Error).message
+          }`
+        );
+      }
+    },
+  };
+}
